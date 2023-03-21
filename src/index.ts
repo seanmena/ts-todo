283461e8a05e92ab81f8d7897dba4192301b5a7e
Sean Mena <seanmena13@gmail.com>
2023-03-21 11:45:17 -0600

    lil ts pr

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,61 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidv4 } from "uuid";
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string, 
+  title: string, 
+  completed: boolean, 
+  createdAt: Date 
+} 
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>("#list");
+// for ones that don't let you do the angle brackets:
+// const list = document.getElementById("list") as HTMLUListElement | null 
+const form = document.querySelector<HTMLFormElement>("#new-task-form");
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  if (input?.value == "" || input?.value == null) return
+
+  const newTask: Task = {
+    id: uuidv4(),
+    title: input.value, 
+    completed: false,
+    createdAt: new Date()
+  }
+
+  tasks.push(newTask)
+  tasks.push()
+
+  addListItem(newTask)
+  input.value = ""
+})
+
+function addListItem(task: Task){
+  const item = document.createElement("li")
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
+
+function saveTasks() {
+  const activeTasks = tasks.filter(task => !task.completed)
+  localStorage.setItem("TASKS", JSON.stringify(activeTasks))
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS")
+  if (taskJSON == null) return []
+  return JSON.parse(taskJSON)
+}
